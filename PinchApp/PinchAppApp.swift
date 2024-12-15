b0d8df6bd2c037e36d8f3c3972e157513788c1e1
Marco <humarco1310@gmail.com>
2024-12-15 16:44:32 +0100

    Initial Commit

---
+++ PinchApp/PinchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchAppApp.swift
+//  PinchApp
+//
+//  Created by Marco Hu on 15/12/24.
+//
+
+import SwiftUI
+
+@main
+struct PinchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
